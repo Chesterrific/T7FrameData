83c1c58a7116539c2a8a13c5bf88f02bdb9e1100
Chesterrific <chesterguinto@gmail.com>
2018-06-07 19:10:00 -0700

    Added various images
    Started on general web design

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,11 +1,20 @@
-var xhttp = new XMLHttpRequest();
+var names = [];
+
+/*var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    GetCharacter(btn.value);
+    console.log(btn.value);
+})
+*/
+function GetCharacter(name) {
+    var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             var myData = JSON.parse(xhttp.responseText);
-            RenderHTML(myData);
+            console.log(myData.name);
         }
     };
-    xhttp.open("GET", "" + counter + ".json", true);
+    xhttp.open("GET", "frames/24_" + name + ".json", true);
     xhttp.send();
-
-
+}
