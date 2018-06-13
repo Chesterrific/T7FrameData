0ba0985c555bdf49e6968cba727f60dc2cc0e844
Chesterrific <chesterguinto@gmail.com>
2018-06-13 14:44:30 -0700

    Added the rest of the roster
    Roster, as well as the beginnings of side bar navigation have been added.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,13 +1,4 @@
-var names = [];
-
-/*var btn = document.getElementById("btn");
-
-btn.addEventListener("click", function(){
-    GetCharacter(btn.value);
-    console.log(btn.value);
-})
-*/
-function GetCharacter(name) {
+function getCharacter(name) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
@@ -18,3 +9,25 @@ function GetCharacter(name) {
     xhttp.open("GET", "frames/24_" + name + ".json", true);
     xhttp.send();
 }
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav(name) {
+    var sidebarL = document.getElementById("leftNav");
+    var sidebarR = document.getElementById("rightNav");
+    
+    sidebarL.style.width = "70%";
+    sidebarR.style.width = "30%";
+    
+    sidebarL.innerHTML += '<img src="chars/' + name + '_thumbnail.png" class="img-fluid">';
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    var sidebarL = document.getElementById("leftNav");
+    var sidebarR = document.getElementById("rightNav");
+    
+    sidebarL.style.width = 0;
+    sidebarR.style.width = 0;
+    
+    sidebarL.innerHTML = '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>';
+} 
