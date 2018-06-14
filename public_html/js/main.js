6ccace91c536af905498ca609a3a828c34573665
Chesterrific <chesterguinto@gmail.com>
2018-06-14 14:07:26 -0700

    Added geese and noctis frame data
    Geese and noctis added. Beginning work on adding gifs for moves.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -9,17 +9,17 @@ function getFrameData(name) {
     };
     xhttp.open("GET", "frames/" + name + ".json", true);
     xhttp.send();
-
-
 }
 
 /*Changes nav style width to open side nav*/
 function openNav(name) {
+    document.body.style.overflow = "hidden"; //Disable scrolling on main page
+
     var sidebarL = document.getElementById("leftNav");
     var sidebarR = document.getElementById("rightNav");
 
-    sidebarL.style.width = "55%";
-    sidebarR.style.width = "45%";
+    sidebarL.style.width = "50%";
+    sidebarR.style.width = "50%";
 
     sidebarL.innerHTML += '<img src="chars/' + name + '_thumbnail.png" class="img-fluid">';
 
@@ -28,6 +28,8 @@ function openNav(name) {
 
 /*Changes nav style width to 0 to close side nav*/
 function closeNav() {
+    document.body.style.overflow = "scroll"; //Enable scrolling on main page
+
     var sidebarL = document.getElementById("leftNav");
     var sidebarR = document.getElementById("rightNav");
     var tableContents = document.getElementById("moves");
@@ -43,8 +45,8 @@ function closeNav() {
 function populateMoves(myData) {
     var table = document.getElementById("moves");
 
-    table.innerHTML += '<td colspan="7" style="text-align: center;">SPECIAL MOVES</td>'
     /*For special moves*/
+    table.innerHTML += '<td colspan="7" style="text-align: center;">SPECIAL MOVES</td>';
     for (var i in myData.moves.special) {
         var row = table.insertRow(-1);
         var command = row.insertCell(0); //command cell
@@ -54,18 +56,54 @@ function populateMoves(myData) {
         var hit = row.insertCell(4); //hit cell
         var cH = row.insertCell(5); //counterhit cell
         var notes = row.insertCell(6); //notes cell
-        
+
         command.innerHTML = myData.moves.special[i].Command;
-        hitlevel.innerHTML = myData.moves.special[i]["Hit level"];
-        startup.innerHTML = myData.moves.special[i]["Start up frame"];
-        block.innerHTML = myData.moves.special[i]["Block frame"];
-        hit.innerHTML = myData.moves.special[i]["Hit frame"];
-        cH.innerHTML = myData.moves.special[i]["Counter hit frame"];
-        notes.innerHTML = myData.moves.special[i]["Notes"];
+        
+        /*Error handling for missing "Hit level" fields*/
+        if (myData.moves.special[i]["Hit level"] === undefined) {
+            hitlevel.innerHTML = "";
+        } else {
+            hitlevel.innerHTML = myData.moves.special[i]["Hit level"];
+        }
+        
+        /*Error handling for missing "Start up frame" fields*/
+        if (myData.moves.special[i]["Start up frame"] === undefined) {
+            startup.innerHTML = "";
+        } else {
+            startup.innerHTML = myData.moves.special[i]["Start up frame"];
+        }
+        
+        /*Error handling for missing "On block" fields*/
+        if (myData.moves.special[i]["Block frame"] === undefined) {
+            block.innerHTML = "";
+        } else {
+            block.innerHTML = myData.moves.special[i]["Block frame"];
+        }
+        
+        /*Error handling for missing "Hit frame" fields*/
+        if (myData.moves.special[i]["Hit frame"] === undefined) {
+            hit.innerHTML = "";
+        } else {
+            hit.innerHTML = myData.moves.special[i]["Hit frame"];
+        }
+        
+        /*Error handling for missing "Counter hit frame" fields*/
+        if (myData.moves.special[i]["Counter hit frame"] === undefined) {
+            cH.innerHTML = "";
+        } else {
+            cH.innerHTML = myData.moves.special[i]["Counter hit frame"];
+        }
+        
+        /*Error handling for missing "Notes" fields*/
+        if (myData.moves.special[i]["Notes"] === undefined) {
+            notes.innerHTML = "";
+        } else {
+            notes.innerHTML = myData.moves.special[i]["Notes"];
+        }
     }
 
-    table.innerHTML += '<td colspan="7" style="text-align: center;">BASIC MOVES</td>'
     /*For basic moves*/
+    table.innerHTML += '<td colspan="7" style="text-align: center;">BASIC MOVES</td>';
     for (var i in myData.moves.basic) {
         var row = table.insertRow(-1);
         var command = row.insertCell(0); //command cell
@@ -75,14 +113,34 @@ function populateMoves(myData) {
         var hit = row.insertCell(4); //hit cell
         var cH = row.insertCell(5); //counterhit cell
         var notes = row.insertCell(6); //notes cell
-        
+
         command.innerHTML = myData.moves.basic[i].Command;
         hitlevel.innerHTML = myData.moves.basic[i]["Hit level"];
         startup.innerHTML = myData.moves.basic[i]["Start up frame"];
-        block.innerHTML = myData.moves.basic[i]["Block frame"];
-        hit.innerHTML = myData.moves.basic[i]["Hit frame"];
-        cH.innerHTML = myData.moves.basic[i]["Counter hit frame"];
-        notes.innerHTML = myData.moves.basic[i]["Notes"];
+      
+        if (myData.moves.basic[i]["Block frame"] === undefined) {
+            block.innerHTML = "";
+        } else {
+            block.innerHTML = myData.moves.basic[i]["Block frame"];
+        }
+        
+        if (myData.moves.basic[i]["Hit frame"] === undefined) {
+            hit.innerHTML = "";
+        } else {
+            hit.innerHTML = myData.moves.basic[i]["Hit frame"];
+        }
+        
+        if (myData.moves.basic[i]["Counter hit frame"] === undefined) {
+            cH.innerHTML = "";
+        } else {
+            cH.innerHTML = myData.moves.basic[i]["Counter hit frame"];
+        }
+        
+        if (myData.moves.basic[i]["Notes"] === undefined) {
+            notes.innerHTML = "";
+        } else {
+            notes.innerHTML = myData.moves.basic[i]["Notes"];
+        }
     }
     //console.log(myData.moves.special[1].Command);     //practice on how to access the JSON files.
 }
